b0787de9a5e33ba774ddcae3102a656d9b75d980
Harjot <hlotey@livexchange.com>
2024-01-22 16:10:23 -0800

    CD Test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-orange-700 text-4xl">Welcome to BestBite</h1>
-      <div>New website coming soon</div>
+      <div>New website - coming soon</div>
     </>
   );
 }
