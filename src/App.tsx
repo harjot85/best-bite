2eaa1a23c95da98baa8779dd61f66590366e5895
Harjot <hlotey@livexchange.com>
2024-01-22 16:06:31 -0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-700 text-4xl">Welcome to BestBite</h1>
+      <div>New website coming soon</div>
+    </>
+  );
+}
+
+export default App;
