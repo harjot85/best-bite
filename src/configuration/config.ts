50148a43eaed27978d435a7a5f76a9c596f46f5f
Harjot <hlotey@livexchange.com>
2024-01-30 19:51:26 -0800

    Add Slider for Testionials

---
+++ src/configuration/config.ts
@@ -0,0 +1,30 @@
+export const reponsiveSliderSettings = [
+  {
+    breakpoint: 3000,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 1800,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 1148,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 680,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+];
--- src/pages/testimonials-page.tsx
+++ src/pages/testimonials-page.tsx
@@ -1,17 +1,35 @@
 import { RedBar } from "./home-page";
 import data from "../data/data.json";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { reponsiveSliderSettings } from "../configuration/config";
 
 const TestimonialsPage = () => {
+  const sliderSettings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: reponsiveSliderSettings,
+  };
+
   const { testimonials } = data.business;
+
   return (
     <>
       <div className="text-slate-950 mt-20 pt-8 px-12 min-h-[75vh] lg:min-h-[75vh]">
         <div className="text-4xl">
           <p className="text-red-900 mb-4">Testimonials</p>
           <RedBar height="h-1" />
-          <div className="py-12 text-lg lg:text-xl grid lg:grid-cols-3 lg:gap-6">
-            {testimonials &&
-              testimonials.map((t) => <Testimonial key={t} testimonial={t} />)}
+          <div className="py-12 text-lg lg:text-xl ">
+            <Slider {...sliderSettings}>
+              {testimonials &&
+                testimonials.map((t) => (
+                  <Testimonial key={t} testimonial={t} />
+                ))}
+            </Slider>
           </div>
         </div>
       </div>
@@ -23,7 +41,7 @@ export default TestimonialsPage;
 
 const Testimonial = ({ testimonial }: { testimonial: string }) => {
   return (
-    <div className=" border-solid border-2 border-red-950 pt-8 my-2">
+    <div className=" border-solid border-2 border-red-950 pt-8 m-2 min-h-[32rem]">
       <div className="grid grid-cols-12">
         <div className="col-span-2"></div>
         <div className="col-span-10">
