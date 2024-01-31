b3d2c77f606ea31206b9fe2a4d165bebc6069a32
harjot <harjot.lotey@teachingstrategies.com>
2024-01-30 20:51:52 -0800

    Image section Height adjustment for phone

--- src/pages/gallery-page.tsx
+++ src/pages/gallery-page.tsx
@@ -22,7 +22,7 @@ const GalleryPage = () => {
 
   const images = [P1, P2, P3, P4, P5, B1, I1, I2, S1];
   return (
-    <div id="gallery" className="text-slate-950 mt-20 pt-8 px-12 min-h-[75vh] lg:min-h-[75vh]">
+    <div id="gallery" className="text-slate-950 mt-20 pt-8 px-12 min-h-[60vh] lg:min-h-[75vh]">
       <div className="text-4xl">
         <p className="text-red-900 mb-4">Gallery</p>
         <RedBar height="h-1" />
