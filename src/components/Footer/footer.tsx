67908c705581d9da0022cca8abf7aa2ae6d6ee2f
Harjot <hlotey@livexchange.com>
2024-01-31 18:39:44 -0800

    Added smooth scroll | Added more menu items

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -9,12 +9,8 @@ export const Footer = () => {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="https://flowbite.com/" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8 me-3"
-                alt="FlowBite Logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+             
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-red-800">
                 {name}
               </span>
             </a>
