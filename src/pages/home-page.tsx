609fbed8db598e9e6817796d0e9d274847a77439
Harjot <hlotey@livexchange.com>
2024-01-31 19:18:02 -0800

    Update cover photo

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/navigation/Navbar";
-import wings from "../assets/wings-min.jpg";
+import wings from "../assets/p4.jpg";
 import data from "../data/data.json";
 
 const HomePage = () => {
@@ -33,11 +33,11 @@ const MainCover = ({ phoneNumber, image }: MainCover) => {
 type Phone = { phoneNumber: string };
 const PhoneNumber = ({ phoneNumber }: Phone) => {
   return (
-    <div className="flex justify-start -mt-32 lg:-mt-24 ml-8 lg:ml-16 ">
+    <div className="flex justify-start -mt-24 lg:-mt-24 ml-8 lg:ml-16 ">
       <span
-        className=" text-red-800 text-lg font-semibold md:text-lg
-                          border-solid border-2 border-red-900 rounded-full
-                          px-6 lg:px-12 py-2 lg:py-3"
+        className=" bg-red-800 text-lg font-semibold md:text-2xl
+                          border-solid border-2 border-red-950 rounded-full
+                          px-6 lg:px-16 py-2 lg:py-4"
       >
         <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
       </span>
