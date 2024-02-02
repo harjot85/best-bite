19251fe8ee41516004a53987248b64756427fdb4
Harjot <hlotey@livexchange.com>
2024-02-02 14:53:15 -0800

    OS detection Test

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import "@fontsource/chakra-petch";
 function App() {
   return (
     <>
-      
-      {isUnsupportedDevice() && <div className="bg-red-500 p-48">Hey this is it! </div>}
+      {console.log("isWindows10OrLater", isWindows10OrLater())}
+      {!isWindows10OrLater() && (
+        <div className="bg-red-500 p-48">Hey this is it! </div>
+      )}
       <BestBiteApp />
     </>
   );
@@ -13,50 +15,14 @@ function App() {
 
 export default App;
 
-
-const checkOperatingSystem = () => {
-  let operatingSystem = 'Unknown OS';
-  //const isMac = navigator.userAgent.toLowerCase().includes('mac');
-  //if (isMac) return
+const isWindows10OrLater = () => {
   if (navigator.userAgent) {
-    const platformMatch = navigator.userAgent.match(/(Windows NT \d+\.\d+|Mac OS X \d+_\d+_\d+|Linux)/i);
+    const platformMatch = navigator.userAgent.match(/(Windows NT \d+\.\d+)/i);
+    console.log("platformMatch", platformMatch);
     if (platformMatch) {
-      operatingSystem = platformMatch[0];
-      if (operatingSystem.startsWith('Windows NT')) {
-        const versionMap: { [key: string]: string } = {
-          '10.0': '10',
-          '6.3': '8.1',
-          '6.2': '8',
-          '6.1': '7',
-          '6.0': 'Vista',
-          '5.2': 'Server 2003/XP x64',
-          '5.1': 'XP',
-          '5.0': '2000',
-        };
-        const version = operatingSystem.substring(11);
-        operatingSystem = 'Windows ' + (versionMap[version] || version);
-      } else if (operatingSystem.startsWith('Mac OS X')) {
-        operatingSystem = operatingSystem.replace(/_/g, '.').replace('Mac OS X', 'MacOS');
-      }
+      const version = parseFloat(platformMatch[0].split(" ")[2]);
+      return version >= 10;
     }
   }
-  console.log("OS:", operatingSystem)
-  return operatingSystem;
+  return false;
 };
-
-const getDeviceType = () => {
-  const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-  const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  const tablet = /Tablet|iPad/i.test(userAgent);
-
-  if (mobile) {
-    return 'Mobile';
-  } else if (tablet) {
-    return 'Tablet';
-  } else {
-    return 'Computer';
-  }
-};
-
-const isUnsupportedDevice = () => getDeviceType() === 'Mobile' || getDeviceType() === 'Tablet';
-!checkOperatingSystem() || (checkOperatingSystem() && !checkOperatingSystem().includes('Windows'));
