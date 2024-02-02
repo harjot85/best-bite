66385710cf9a0dac729fbf2d2f980c5621179ef2
Harjot <hlotey@livexchange.com>
2024-02-02 14:59:08 -0800

    Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,7 @@ import BestBiteApp from "./pages/bestbite-app";
 import "@fontsource/chakra-petch";
 
 function App() {
-  return (
-    <>
-      {console.log("isWindows10OrLater", isWindows10OrLater())}
-      {!isWindows10OrLater() && (
-        <div className="bg-red-500 p-48">Hey this is it! </div>
-      )}
-      <BestBiteApp />
-    </>
-  );
+  return <BestBiteApp />;
 }
 
 export default App;
-
-const isWindows10OrLater = () => {
-  if (navigator.userAgent) {
-    const platformMatch = navigator.userAgent.match(/(Windows NT \d+\.\d+)/i);
-    console.log("platformMatch", platformMatch);
-    if (platformMatch) {
-      const version = parseFloat(platformMatch[0].split(" ")[2]);
-      return version >= 10;
-    }
-  }
-  return false;
-};
