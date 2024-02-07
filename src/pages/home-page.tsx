0572dd9f153b170cb6b41931f6f49f75fd97917d
harjot <harjot.lotey@teachingstrategies.com>
2024-02-06 20:48:52 -0800

    Home Page Slider

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,15 +1,22 @@
 import Navbar from "../components/navigation/Navbar";
-import Cover from "../assets/images/3.jpg";
 import data from "../data/data.json";
+import Slider from "react-slick";
+import Image1 from "../assets/images/1.jpg";
+import Image2 from "../assets/images/3.jpg";
+import Image3 from "../assets/images/4.jpg";
+import Image4 from "../assets/images/5.jpg";
+import Image5 from "../assets/images/7.jpg";
+import Image6 from "../assets/images/9C.jpg";
 
 const HomePage = () => {
+  const images = [Image1, Image2, Image3, Image4, Image5, Image6];
   return (
     <>
       <RedBar height="h-1" />
       <Navbar />
       <MainCover
         phoneNumber={data.business.phoneNumber}
-        image={Cover}
+        images={images}
         subText={data.business.servingYou}
       />
     </>
@@ -23,17 +30,41 @@ export const RedBar = ({ height }: RedBar) => {
   return <div className={`w-full ${height} bg-orange-800`} />;
 };
 
-type MainCover = { image: string; phoneNumber: string; subText: string };
+type MainCover = { images: string[]; phoneNumber: string; subText: string };
 
-const MainCover = ({ phoneNumber, image, subText }: MainCover) => {
+const MainCover = ({ phoneNumber, images, subText }: MainCover) => {
+  const sliderSettings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    autoplay: true,
+    arrows: false,
+    infinite: true,
+
+  };
+
   return (
     <div id="main-image flex">
-      <img src={image} className="w-full h-[90vh] object-cover" />
-      <div className="text-orange-400 flex justify-start -mt-24 lg:-mt-24 ml-8 lg:ml-28">
-        <em>{subText}</em>
+      <Slider {...sliderSettings}>
+        {images.map((image, i) => {
+          return (
+            <img
+              key={i}
+              src={image}
+              alt=""
+              className="w-full h-[87vh] object-cover"
+            />
+          );
+        })}
+      </Slider>
+      <div className="text-orange-400 flex justify-start -mt-24 lg:-mt-24 ml-8 lg:ml-28 relative">
+        <span className="bg-black px-4 py-2 rounded-md">
+          <em>{subText}</em>
+        </span>
       </div>
 
-      <span className="flex justify-start -mt-28 lg:-mt-28 ml-8 lg:ml-16 ">
+      <span className="flex justify-start -mt-28 lg:-mt-28 ml-8 lg:ml-16 relative">
         <PhoneNumber phoneNumber={phoneNumber} />
       </span>
     </div>
